017637beba53b8edf53a0d9e623deef38970865d
Peter Leach <peter.g.leach@gmail.com>
2014-03-02 14:12:50 -0800

    working with oscilator

---
+++ ReactivisionUnityProject/Assets/scripts/OSC_Controller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OSC_Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		OSCHandler.Instance.Init ();
+
+		OSCHandler.Instance.SendMessageToClient ("HolodeckClient", "address/folder/1", 13);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
